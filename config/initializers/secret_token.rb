4f5ff822fd445e45d945886238092eba62a2d516
Jørgen Orehøj Erichsen <joe@erichsen.net>
2010-10-11 09:07:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TransloaditPaperclipExample::Application.config.secret_token = '855391af88ad51b3bac7e80ffbb13b4d408d36cc55e2469d02ac1557acc28d307c56c908e19068f989d4018b2032eb858d9973c8795e73d92d192518b267895d'
